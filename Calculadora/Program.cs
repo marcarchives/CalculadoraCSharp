32abb7594f40325c866c5376223cfe16402bba2e
Leonardo Vieira <leonardovieira295@gmail.com>
2025-02-28 14:17:39 -0300

    multiplicação e divisão adicionados

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int valor1 = 10, valor2 = 10;
+
+            int div = valor1 / valor2;
+            int mult = valor1 * valor2;
+
+            Console.WriteLine("Escolha uma operação");
+
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+
+
+            int operacao = int.Parse(Console.ReadLine());
+
+            if (operacao == 3)
+            {
+                Console.WriteLine($"Você escolhei a opção: {operacao} - Divisão: 10 / 10  = {div}");
+            }
+
+            else if (operacao == 4)
+            {
+                Console.WriteLine($"Você escolheu a opção: {operacao} - Multiplicação: 10 * 10 = {mult}");
+
+            }
+            else
+            {
+                Console.WriteLine("Você escolheu uma opção inválida");
+            }
         }
     }
 }
